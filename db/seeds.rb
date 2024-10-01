ea113b1f2d24a7bf0c17d6b4d12ab742b59c5238
Adam Miribyan <321133+adammiribyan@users.noreply.github.com>
2024-10-01 13:39:13 +0200

    Add sample posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,42 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Clear existing posts
+Post.destroy_all
+
+# Create an array of external websites
+external_sites = [
+  'https://rubyonrails.org',
+  'https://github.com',
+  'https://developer.mozilla.org',
+  'https://www.ruby-lang.org',
+  'https://tailwindcss.com'
+]
+
+# Create 10 posts with interlinks and external links
+10.times do |i|
+  title = "Post #{i + 1}: #{Faker::Lorem.sentence(word_count: 3)}"
+
+  # Generate body content with internal and external links
+  body = ""
+
+  # Add paragraphs
+  3.times do
+    body += "<p>#{Faker::Lorem.paragraph(sentence_count: 3)}</p>"
+  end
+
+  # Add an internal link (if not the first post)
+  if i > 0
+    internal_link_id = rand(1..i)
+    body += "<p>Check out <a href='/posts/#{internal_link_id}'>this related post</a>.</p>"
+  end
+
+  # Add an external link
+  external_link = external_sites.sample
+  body += "<p>For more information, visit <a href='#{external_link}' target='_blank'>this external resource</a>.</p>"
+
+  # Create the post
+  Post.create!(
+    title: title,
+    body: ActionText::RichText.new(body: body)
+  )
+end
+
+puts "Created #{Post.count} posts with interlinks and external links."
